28c293161e54a2afa7b72c24f64fbb8fe6b02c94
Saúl Cabrera <saulecabrera@gmail.com>
2019-10-06 11:36:37 -0400

    First commit

---
+++ lib/markdownif.ex
@@ -0,0 +1,18 @@
+defmodule Markdownif do
+  @moduledoc """
+  Documentation for Markdownif.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Markdownif.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/markdownif_test.exs
@@ -0,0 +1,8 @@
+defmodule MarkdownifTest do
+  use ExUnit.Case
+  doctest Markdownif
+
+  test "greets the world" do
+    assert Markdownif.hello() == :world
+  end
+end
