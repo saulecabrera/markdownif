bb63987d65675897eaf41055b42c5741d17bd162
Saúl Cabrera <saulecabrera@gmail.com>
2019-10-09 20:39:19 -0400

    Add rustler

--- mix.exs
+++ mix.exs
@@ -7,22 +7,30 @@ defmodule Markdownif.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      compilers: [:rustler] ++ Mix.compilers,
+      rustler_crates: crates(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:rustler, "~> 0.21.0"},
+    ]
+  end
+
+  defp crates do
+    [
+      markdownif: [
+        path: "native/markdownif",
+        features: [],
+      ]
     ]
   end
 end
