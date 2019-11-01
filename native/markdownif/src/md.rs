22b511a3620ca156b97fc2f62091a2cac605d37d
Saúl Cabrera <saulecabrera@gmail.com>
2019-11-01 09:23:44 -0400

    Added Features struct and allow conditional features

--- native/markdownif/src/md.rs
+++ native/markdownif/src/md.rs
@@ -1,15 +1,43 @@
 use crate::atoms;
-use rustler::{Env, Error, Term, Encoder};
+use rustler::{Env, Error, Term, Encoder, NifStruct};
 use pulldown_cmark::{Parser, Options, html};
 
-pub fn to_html<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error>  {
-    args[0]
-        .decode()
-        .and_then(|input: String| {
-            let parser = Parser::new_ext(&input, Options::empty());
-            let mut output = String::new();
-            html::push_html(&mut output, parser);
+#[derive(NifStruct)]
+#[module = "Markdownif.Features"]
+struct Features {
+    pub tables: bool,
+    pub footnotes: bool,
+    pub strikethrough: bool,
+    pub tasklists: bool,
+}
 
-            Ok(output.encode(env))
-        })
+pub fn to_html<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error>  {
+    let input: &str = args[0].decode()?;
+    let features: Features = args[1].decode()?;
+
+
+    let mut opts = Options::empty();
+    if features.strikethrough {
+        opts.insert(Options::ENABLE_STRIKETHROUGH);
+    }
+
+    if features.tasklists {
+        opts.insert(Options::ENABLE_TASKLISTS);
+    }
+
+    if features.footnotes {
+        opts.insert(Options::ENABLE_FOOTNOTES);
+    }
+
+    if features.tables {
+        opts.insert(Options::ENABLE_TABLES);
+    }
+
+    let mut output = String::new();
+    html::push_html(&mut output, Parser::new_ext(
+        &input,
+        opts
+    ));
+
+    Ok(output.encode(env))
 }
