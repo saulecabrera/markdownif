fbfc66c6c506201766732159652f563853bbe752
Saúl Cabrera <saulecabrera@gmail.com>
2019-11-20 07:06:37 -0500

    v0.9.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Markdownif.MixProject do
   def project do
     [
       app: :markdownif,
-      version: "0.1.0",
+      version: "0.9.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       compilers: [:rustler] ++ Mix.compilers,
