3953a22e18c6d47ae14739ec5e630c7d7fec700e
Saúl Cabrera <saulecabrera@gmail.com>
2019-11-22 10:27:57 -0500

    Add tests for basic functionality and optional features

--- test/markdownif_test.exs
+++ test/markdownif_test.exs
@@ -1,8 +1,60 @@
 defmodule MarkdownifTest do
+  alias Markdownif.Features
   use ExUnit.Case
   doctest Markdownif
 
-  test "greets the world" do
-    assert Markdownif.hello() == :world
+  setup do
+    tasklist = """
+    - [ ] A
+    """
+
+    strikethrough = "~~striked~~"
+
+    table = """
+    | Tables        | Are           | Cool  |
+    | ------------- |:-------------:| -----:|
+    | foo           | bar           |    $1 |
+    """
+
+    footnote = """
+    Lorem ipsum.[^a]
+
+    [^a]: Definition.
+    """
+
+    [
+      tasklist: tasklist,
+      strikethrough: strikethrough,
+      table: table,
+      footnote: footnote
+    ]
+  end
+
+  describe "to_html/2" do
+    test "additional markdown features are disabled by default", context do
+      tasklist = Markdownif.to_html(context[:tasklist])
+      refute String.contains?(tasklist, ["input", "type", "checkbox"])
+
+      strikethrough = Markdownif.to_html(context[:strikethrough])
+      refute String.contains?(strikethrough, ["<del>", "</del>"])
+
+      table = Markdownif.to_html(context[:table])
+      refute String.contains?(table, ["<table>", "</table>"])
+    end
+
+    test "tasklists are correctly rendered", context do
+      tasklist = Markdownif.to_html(context[:tasklist], %Features{tasklists: true})
+      assert String.contains?(tasklist, ["input", "type", "checkbox"])
+    end
+
+    test "strikethrough is correctly rendered", context do
+      striked = Markdownif.to_html(context[:strikethrough], %Features{strikethrough: true})
+      assert String.contains?(striked, ["<del>", "</del>"])
+    end
+
+    test "tables are correctly rendered", context do
+      table = Markdownif.to_html(context[:table], %Features{tables: true})
+      assert String.contains?(table, ["<table>", "</table>"])
+    end
   end
 end
